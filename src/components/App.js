604210feef0902d2880cf2cc42b42828d09958dc
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-04-19 21:49:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,29 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      enrollmentNo: "12345678",
+      age: 34
+    };
+  }
+
+  increaseAge = () => {
+    this.setState({ age: this.state.age + 1 });
+  }
+
+  render() {
+    const { name, enrollmentNo, age } = this.state; // Destructure state values
+    return (
+      <div>
+        <h1>Hello, my name is {name}</h1>
+        <p>I am {age} years old and my enrollment no is {enrollmentNo}</p>
+        <button onClick={this.increaseAge}>Increase Age</button>
+      </div>
+    );
+  }
 }
 
 
